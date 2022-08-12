9719e031c3116925483e249395707dc7911d7d1d
Max <58471780+adminatgithub@users.noreply.github.com>
2022-08-12 14:19:36 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,33 @@
+def encode(index, size):
+  vec = [0. for i in range(size)]
+  vec[index] = 1.
+  return vec
+
+def decode(vec):
+  index = None
+  h = 0
+  for i,v in enumerate(vec[0]):
+    if h<v:
+      h = v
+      index = i
+  return index
+
+def batch_encode(indexes, size):
+  vectors = []
+  for i in range(len(indexes)):
+    vec = np.zeros((size))
+    vec[indexes[i]] = 1
+    vectors = np.append(vectors, vec)
+  return vectors.reshape(-1, size)
+
+def batch_decode(vectors):
+  indexes = []
+  for vec in vectors:
+    index = None
+    h = 0
+    for i,v in enumerate(vec):
+      if h<v:
+        h = v
+        index = i
+    indexes.append(index)
+  return indexes
