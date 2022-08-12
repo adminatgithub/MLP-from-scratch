b9be57f9ff5db25488ae55704df0b80bfb4115cd
Max <58471780+adminatgithub@users.noreply.github.com>
2022-08-12 14:30:39 +0200

    Update mnist-example.py

--- mnist-example.py
+++ mnist-example.py
@@ -1,9 +1,24 @@
-import numpy as np
-import random
 from keras.datasets import mnist
-import timeit
+from mlp import MLP
+from layer import *
+from activations import *
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train/255.*2.-1.
 x_test = x_test/255.*2.-1.
+
+
+layers = [
+  InputLayer(),
+  Flatten(),
+  DenseLayer(784, 60),
+  Relu(),
+  DenseLayer(60, 10),
+  Sigmoid(),
+]
+
+mlp = MLP(layers)
+
+
+mlp.fit(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test,lr=0.01, epochs=10, batch_size=128))
