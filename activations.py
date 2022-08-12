976c458363b930f7904f31aa6d2e35ee44ddd2aa
Max <58471780+adminatgithub@users.noreply.github.com>
2022-08-12 14:21:47 +0200

    Create activations.py

---
+++ activations.py
@@ -0,0 +1,14 @@
+class Sigmoid:
+  def __init__(self):
+      self.x = None
+      self.y = None
+      self.trainable = False
+      self.flatten = False
+
+  def forward(self, x, batch_size):
+    self.x = x
+    self.y = 1 / (1 + np.exp(-x))
+    return self.y
+
+  def dY_dX(self):
+    return (1-self.y)*self.y
